be665c9996033c9dbbfe1718426c162510eb1684
Omar Hussein <omarelwy@hotmail.com>
2022-06-20 19:05:26 +0200

    🚀 Readd setting eb profile file for

--- udagram/udagram-api/scripts/deploy.sh
+++ udagram/udagram-api/scripts/deploy.sh
@@ -2,6 +2,16 @@
 set -x
 set -e
 
+AWS_CONFIG_FILE=$HOME/.aws/config
+
+touch $AWS_CONFIG_FILE
+chmod 600 $AWS_CONFIG_FILE
+
+echo "[profile cli]"                                > $AWS_CONFIG_FILE
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID"         >> $AWS_CONFIG_FILE
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> $AWS_CONFIG_FILE
+
+
 eb setenv AWS_PROFILE=$AWS_PROFILE
 eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
 eb setenv AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
@@ -14,6 +24,5 @@ eb setenv POSTGRES_HOST=$POSTGRES_HOST
 eb setenv POSTGRES_PASSWORD=$POSTGRES_PASSWORD
 eb setenv POSTGRES_USERNAME=$POSTGRES_USERNAME
 eb setenv URL=$URL
-
 
 eb deploy
